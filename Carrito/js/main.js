61d574803da723f5ab232b06b0ea5a3883565791
Luis-Hdez <lf.hdez@hotmail.com>
2025-07-23 20:23:33 -0600

    Funcionalidades de JS:  Eliminar carrito, eliminar curso, agregar curso con su precio

--- Carrito/js/main.js
+++ Carrito/js/main.js
@@ -5,17 +5,61 @@ let cursoCarrito = [];
 
 function vaciarCarrito(evento) {
   console.log("Soy el vacia carrito");
+
+  cursoCarrito = [];
+  contenedorCarrito.innerHTML = "";
+}
+
+function eliminarCurso(id) {
+  console.log(id);
+
+  cursoCarrito.map((curso) => {
+    if (curso.id == id) {
+      cursoCarrito = cursoCarrito.filter((curso) => curso.id != id);
+    }
+  });
+
+  recorrerCarritoHTML();
+
+  // const cursoId = evento.target.getAttribute("data-id");
+  // cursoCarrito = cursoCarrito.filter((curso) => curso.id !== cursoId);
+  // recorrerCarritoHTML();
 }
 
 function agregarCurso(evento) {
   console.log("Agregar Curso");
 
   let curso = extraerDatosCurso(evento.target.parentElement.parentElement);
-
   console.log(curso);
 
-  cursoCarrito.push(curso);
-  console.log(cursoCarrito);
+  // CHequeamos si el curso existe previamente, guardamos true o false
+  const existe = cursoCarrito.some((cursoArr) => cursoArr.id === curso.id);
+
+  if (existe === true) {
+    cursoCarrito.map((cursoArr) => {
+      if (cursoArr.id === curso.id) {
+        // aumentar precio
+        // utilizar un metdo de string que pueda el quitar el primer caracter
+        // metodos prosibles Substring o Slice
+        cursoArr.cantidad += 1;
+        cursoArr.precio = cursoArr.precio.substring(1); // el dato esta en prosicion 0
+
+        // transformamos el string a numero
+        // parseInt or parseFloat
+        cursoArr.precio = parseFloat(cursoArr.precio);
+        cursoArr.cantidad = parseFloat(cursoArr.cantidad);
+
+        let basePrecio = cursoArr.precio / (cursoArr.cantidad - 1);
+        cursoArr.precio = basePrecio * cursoArr.cantidad;
+        cursoArr.precio = `$${cursoArr.precio}`;
+
+        return cursoArr;
+      }
+    });
+  } else {
+    cursoCarrito.push(curso);
+    console.log(cursoCarrito);
+  }
 
   recorrerCarritoHTML();
 }
@@ -54,9 +98,11 @@ function recorrerCarritoHTML() {
       <td>${curso.titulo}</td>
       <td>${curso.precio}</td>
       <td>${curso.cantidad}</td>
-      <td><a class="btn btn-danger">Eliminar</a></td>
+      <td><a class="btn btn-danger" onclick="eliminarCurso(${curso.id})">Eliminar</a></td>
     `;
 
     contenedorCarrito.appendChild(fila);
   });
 }
+
+// <td><a class="btn btn-danger" onclick="eliminarCurso(event)" data-id = ${curso.id}>Eliminar</a></td>
